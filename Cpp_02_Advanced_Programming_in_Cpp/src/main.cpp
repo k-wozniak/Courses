71e795a4eba888b8729682cd8e824726b383dbeb
k-wozniak <kamil.krzysztof.wozniak@outlook.com>
2021-09-08 11:10:54 +0100

    Add Week 10

--- Cpp_02_Advanced_Programming_in_Cpp/src/main.cpp
+++ Cpp_02_Advanced_Programming_in_Cpp/src/main.cpp
@@ -1,41 +1,50 @@
 #include <iostream>
 
 #include "week_9.h"
+#include "week_10.h"
 
 using namespace std;
 
 int main()
 {
-  // **************************************************************
-  // Week 9
-  // **************************************************************
+    // **************************************************************
+    // Week 9
+    // **************************************************************
 
-  //cout << "5 choose 3 gives " << Week9::N_Choose_K(5, 3) << endl;
-  
-  /*
-  int a = 5, b = 3;
-  Week9::Swap_Values(a, b);
-  cout << "A was 5 is: " << a << " B was 3 is: " << b << endl;
-  */
+    //cout << "5 choose 3 gives " << Week9::N_Choose_K(5, 3) << endl;
+    
+    /*
+    int a = 5, b = 3;
+    Week9::Swap_Values(a, b);
+    cout << "A was 5 is: " << a << " B was 3 is: " << b << endl;
+    */
 
-  /*
-  int number = 11125, num_of_digits, sum_of_digits;
-  Week9::Return_By_Reference(number, sum_of_digits, num_of_digits);
-  cout << "For number: " << number
-       << " the sum of Digits is: "<< sum_of_digits 
-       << " the number of digits is: " << num_of_digits << endl;
-  */
+    /*
+    int number = 11125, num_of_digits, sum_of_digits;
+    Week9::Return_By_Reference(number, sum_of_digits, num_of_digits);
+    cout << "For number: " << number
+        << " the sum of Digits is: "<< sum_of_digits 
+        << " the number of digits is: " << num_of_digits << endl;
+    */
 
-  /*
-  cout << "int Do_Something returns " << Week9::do_something(1) << endl;
-  cout << "bool Do_Something returns " << Week9::do_something(true) << endl;
-  cout << "float Do_Something returns " << Week9::do_something(1.1f) << endl;
-  */
+    /*
+    cout << "int Do_Something returns " << Week9::do_something(1) << endl;
+    cout << "bool Do_Something returns " << Week9::do_something(true) << endl;
+    cout << "float Do_Something returns " << Week9::do_something(1.1f) << endl;
+    */
 
-  // **************************************************************
-  // Week 10
-  // **************************************************************
+    // **************************************************************
+    // Week 10
+    // **************************************************************
 
-  
-  return 0;
+    // int mybirthyear = 2000;
+    // if (Week10::Is_Leap_Year(mybirthyear))
+    // {
+    //   printf("Year %i was a leap year.\n", mybirthyear);
+    // }
+
+    // double weigth = 50.0, height = 1.58;
+    // printf("BMI is: %3.2f \n", Week10::Calculate_BMI(weigth, height));
+    
+    return 0;
 }
--- Cpp_02_Advanced_Programming_in_Cpp/src/week_10.cpp
+++ Cpp_02_Advanced_Programming_in_Cpp/src/week_10.cpp
@@ -1,10 +1,26 @@
-#include "week_9.h"
+#include "week_10.h"
 
 #include <iostream>
 // #include <cmath>
 using namespace std;
 
-float Week9::do_something(float x)
+bool Week10::Is_Leap_Year(unsigned short int year)
 {
-    return 4.444;
+    bool div_by_4 = !((bool)(year%4));
+    bool div_by_100 = !((bool)(year%100));
+    bool div_by_400 = !((bool)(year%400));
+
+    if (div_by_400 || (div_by_4 && !div_by_100))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+double Week10::Calculate_BMI(double weight, double height)
+{
+    return weight / (height*height);
 }
